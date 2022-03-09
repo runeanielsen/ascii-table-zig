9dcc824b72304c6bbbd0b14a2862b21b689b232c
Rune Nielsen <runenielsen@runbox.com>
2022-03-09 20:06:41 +0100

    now prints everything at once instead of each table row

--- src/main.zig
+++ src/main.zig
@@ -33,25 +33,35 @@ fn getBodyRow(allocator: mem.Allocator, i: usize) ![]const u8 {
         }
     }
 
+    try list.append('\n');
+
     return list.items;
 }
 
 fn getHeaderRow() []const u8 {
     const header = "Dec  Hex  Oct  C";
-    return (header ++ " | ") ** 3 ++ header;
+    return (header ++ " | ") ** 3 ++ header ++ "\n";
+}
+
+fn getAsciiTable(allocator: mem.Allocator) ![]const u8 {
+    var list = ArrayList(u8).init(allocator);
+
+    const header_row = getHeaderRow();
+    try list.appendSlice(header_row);
+
+    var i: usize = 0;
+    while (i < 32) : (i += 1) {
+        const body_row = try getBodyRow(allocator, i);
+        try list.appendSlice(body_row);
+    }
+
+    return list.items;
 }
 
 pub fn main() anyerror!void {
     var arena = ArenaAllocator.init(heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-
-    const header_row = getHeaderRow();
-    print("{s}\n", .{header_row});
-
-    var i: usize = 0;
-    while (i < 32) : (i += 1) {
-        const body_row = try getBodyRow(allocator, i);
-        print("{s}\n", .{body_row});
-    }
+    const ascii_table = try getAsciiTable(allocator);
+    print("{s}", .{ascii_table});
 }
