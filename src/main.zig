8fe2124b48cad2bd1d221062fa2ec37b6a45e3a0
Rune Nielsen <runenielsen@runbox.com>
2022-04-18 11:15:04 +0200

    rewrite for simplification

--- src/main.zig
+++ src/main.zig
@@ -1,67 +1,113 @@
 const std = @import("std");
 const print = std.debug.print;
-const mem = std.mem;
 const fmt = std.fmt;
-const heap = std.heap;
-const ArrayList = std.ArrayList;
-const ArenaAllocator = heap.ArenaAllocator;
+const testing = std.testing;
+const mem = std.mem;
 
 fn getChar(i: u8) u8 {
-    if (i < 33 or i == 127) {
-        return ' ';
-    } else {
-        return i;
+    return if (i < 33 or i == 127) ' ' else i;
+}
+
+fn tableRows() [32][4]u8 {
+    var rows: [32][4]u8 = undefined;
+    var i: u8 = 0;
+    while (i <= 31) : (i += 1) {
+        rows[i][0] = i;
+        rows[i][1] = i + 32;
+        rows[i][2] = i + 64;
+        rows[i][3] = i + 96;
     }
+    return rows;
 }
 
-fn getBodyRow(allocator: mem.Allocator, i: usize) ![]const u8 {
-    var list = ArrayList(u8).init(allocator);
-
-    var j: usize = 0;
-    while (j < 4) : (j += 1) {
-        const y = i + j * 32;
-
-        const columns = try fmt.allocPrint(
-            allocator,
-            "{d:>3} {x:>4} {o:>4}  {c}",
-            .{ y, y, y, getChar(@intCast(u8, y)) });
-
-        try list.appendSlice(columns);
-
-        if (j < 3) {
-            try list.appendSlice(" | ");
-        }
+fn bodyRow(allocator: mem.Allocator, tableRow: [4]u8) ![]const u8 {
+    var formattedBlocks: [4][]const u8 = undefined;
+    for (tableRow) |n, i| {
+        formattedBlocks[i] = try formatBlock(allocator, n);
     }
-
-    try list.append('\n');
-
-    return list.items;
+    return try mem.join(allocator, " | ", formattedBlocks[0..]);
 }
 
-fn getHeaderRow() []const u8 {
-    const header = "Dec  Hex  Oct  C";
-    return (header ++ " | ") ** 3 ++ header ++ "\n";
+fn headerRow() []const u8 {
+    return ("Dec  Hex  Oct  C" ++ " | ") ** 3 ++ "Dec  Hex  Oct  C";
 }
 
-fn getAsciiTable(allocator: mem.Allocator) ![]const u8 {
-    var list = ArrayList(u8).init(allocator);
+fn formatBlock(allocator: mem.Allocator, n: u8) ![]const u8 {
+    return try fmt.allocPrint(
+        allocator,
+        "{d:>3} {x:>4} {o:>4}  {c}",
+        .{ n, n, n, getChar(n) },
+    );
+}
 
-    const header_row = getHeaderRow();
-    try list.appendSlice(header_row);
-
-    var i: usize = 0;
-    while (i < 32) : (i += 1) {
-        const body_row = try getBodyRow(allocator, i);
-        try list.appendSlice(body_row);
+fn asciiTable(allocator: mem.Allocator) ![]const u8 {
+    var formattedTableRows: [33][]const u8 = undefined;
+    formattedTableRows[0] = headerRow();
+    for (tableRows()) |row, i| {
+        formattedTableRows[i + 1] = try bodyRow(allocator, row);
+        std.log.warn("{s}", .{formattedTableRows[i + 1]});
     }
-
-    return list.items;
+    return try mem.join(allocator, "\n", formattedTableRows[0..]);
 }
 
-pub fn main() anyerror!void {
-    var arena = ArenaAllocator.init(heap.page_allocator);
+pub fn main() !void {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
-    const ascii_table = try getAsciiTable(allocator);
-    print("{s}", .{ascii_table});
+    print("{s}\n", .{asciiTable(allocator)});
+}
+
+test "create body row" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const expOne = "  0    0    0    |  32   20   40    |  64   40  100  @ |  96   60  140  `";
+    const expTwo = " 22   16   26    |  54   36   66  6 |  86   56  126  V | 118   76  166  v";
+
+    const exampleOneResult = try bodyRow(allocator, [_]u8{ 0, 32, 64, 96 });
+    const exampleTwoResult = try bodyRow(allocator, [_]u8{ 22, 54, 86, 118 });
+
+    try testing.expectEqualStrings(expOne, exampleOneResult);
+    try testing.expectEqualStrings(expTwo, exampleTwoResult);
+}
+
+test "format block" {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const one = "  0    0    0   ";
+    const two = " 32   20   40   ";
+    const three = " 64   40  100  @";
+
+    const blockOne = try formatBlock(allocator, 0);
+    const blockTwo = try formatBlock(allocator, 32);
+    const blockThree = try formatBlock(allocator, 64);
+
+    try testing.expectEqualStrings(one, blockOne);
+    try testing.expectEqualStrings(two, blockTwo);
+    try testing.expectEqualStrings(three, blockThree);
+}
+
+test "getting table rows" {
+    const expRows = [4][4]u8{
+        [_]u8{ 0, 32, 64, 96 },
+        [_]u8{ 1, 33, 65, 97 },
+        [_]u8{ 2, 34, 66, 98 },
+        [_]u8{ 3, 35, 67, 99 },
+    };
+
+    // We just test just the first 4 rows.
+    const rows = tableRows()[0..4];
+
+    try testing.expectEqual(expRows, rows.*);
+}
+
+test "getting character from u8" {
+    try testing.expect(getChar(0) == ' ');
+    try testing.expect(getChar(31) == ' ');
+    try testing.expect(getChar(65) == 'A');
+    try testing.expect(getChar(126) == '~');
+    try testing.expect(getChar(127) == ' ');
 }
